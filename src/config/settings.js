851580a0b48bc01d4f60081792c47eab54047dbf
Michael Chun <michaelchun@Michaels-MacBook-Air.local>
2022-11-24 23:40:34 +0200

    update seo

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,9 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `mckp's photos`,
+	description: `mobile friendly viewer of mckp's photos 
+	+ download`,
+	url: `https://mckp0.github.io/photography/`, // No trailing slash!
+	name: `mckp`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
